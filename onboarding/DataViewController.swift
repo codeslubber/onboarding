fd7db174658177c5236a285ba82c227070897a34
Rob Williams <rob@ontometrics.com>
2015-12-09 14:57:30 -0800

    Initial commit.

---
+++ onboarding/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  onboarding
+//
+//  Created by Rob Williams on 12/9/15.
+//  Copyright © 2015 ontometrics. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
